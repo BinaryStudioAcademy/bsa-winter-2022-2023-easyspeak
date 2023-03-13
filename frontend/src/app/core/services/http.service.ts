01c75a4bef02d09b999b5468adb6902f4f2a8315
Kateryna Herasymiva <kateryna.herasymiva@gmail.com>
2023-03-13 19:05:33 +0200

    15:wip

---
+++ frontend/src/app/core/services/http.service.ts
@@ -0,0 +1,57 @@
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable, throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+
+@Injectable({
+    providedIn: 'root',
+})
+export class HttpService {
+    constructor(private httpClient: HttpClient, private baseUrl: string) {
+    }
+
+    get<T>(url: string): Observable<T[]> {
+        return this.httpClient.get<T[]>(this.buildUrl(url))
+            .pipe(
+                catchError(this.handleError),
+            );
+    }
+
+    getById<T>(url: string, id: string | number): Observable<T> {
+        return this.httpClient.get<T>(`${this.buildUrl(url)}/${id}`)
+            .pipe(
+                catchError(this.handleError),
+            );
+    }
+
+    post<T>(url: string, resource: T): Observable<any> {
+        return this.httpClient.post(this.buildUrl(url), resource)
+            .pipe(
+                catchError(this.handleError),
+            );
+    }
+
+    delete(url: string, id: string | number): Observable<any> {
+        return this.httpClient.delete(`${this.buildUrl(url)}/${id}`)
+            .pipe(
+                catchError(this.handleError),
+            );
+    }
+
+    put<T>(url: string, resource: T): Observable<any> {
+        return this.httpClient.put(this.buildUrl(url), resource)
+            .pipe(
+                catchError(this.handleError),
+            );
+    }
+
+    private handleError(err: HttpErrorResponse): Observable<never> {
+        console.log(err);
+
+        return throwError(() => err);
+    }
+
+    public buildUrl(url: string): string {
+        return this.baseUrl + url;
+    }
+}
