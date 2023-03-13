ad2f7422901b78d7ab5c38162743120813ff21a3
Vladyslav Voropaiev <vladyslav.voropaiev1@nure.ua>
2023-03-13 14:10:24 +0200

    Add lessons component

---
+++ frontend/src/app/modules/lessons/lessons-page/lessons-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-lessons-page',
+  templateUrl: './lessons-page.component.html',
+  styleUrls: ['./lessons-page.component.sass']
+})
+export class LessonsPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
