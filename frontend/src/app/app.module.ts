f7d9ae0f219893bed9ecf12bbb4cdec0246870f2
Vladyslav Voropaiev <vladyslav.voropaiev1@nure.ua>
2023-03-14 20:00:30 +0200

    Install Toastr library

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,13 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { SharedModule } from '@shared/shared.module';
+import { ToastrModule } from 'ngx-toastr';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
     declarations: [AppComponent],
-    imports: [BrowserModule, SharedModule, AppRoutingModule],
+    imports: [BrowserModule, SharedModule, ToastrModule.forRoot(), AppRoutingModule],
     providers: [],
     bootstrap: [AppComponent],
 })
