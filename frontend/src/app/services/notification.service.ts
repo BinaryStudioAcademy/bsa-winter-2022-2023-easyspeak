4289451dcd1552c73758527d10cb00668dc0f25b
Vladyslav Voropaiev <vladyslav.voropaiev1@nure.ua>
2023-03-14 20:02:11 +0200

    Create service for Toastr

---
+++ frontend/src/app/services/notification.service.ts
@@ -0,0 +1,10 @@
+import { Injectable } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class NotificationService {
+
+  constructor(private toastr: ToastrService) { }
+}
