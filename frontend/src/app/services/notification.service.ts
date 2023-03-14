7f066fe261f0abe07f59be4d688647570e4d994a
Vladyslav Voropaiev <vladyslav.voropaiev1@nure.ua>
2023-03-14 20:33:48 +0200

    Create config variable

--- frontend/src/app/services/notification.service.ts
+++ frontend/src/app/services/notification.service.ts
@@ -8,15 +8,27 @@ export class NotificationService {
 
   constructor(private toastr: ToastrService) { }
 
+  private toastrConfig = {
+    closeButton: false,
+    timeOut: 5000,
+    extendedTimeOut: 1000,
+    disableTimeOut: false,
+    easing: 'ease-in',
+    easeTime: 300,
+    progressBar: true,
+    positionClass: 'toast-top-right',
+    tapToDismiss: true
+  }
+
   showSuccess(message: string, title: string) {
-    this.toastr.success(message, title);
+    this.toastr.success(message, title, this.toastrConfig);
   }
 
   showInfo(message: string, title: string) {
-    this.toastr.info(message, title);
+    this.toastr.info(message, title, this.toastrConfig);
   }
 
   showError(message: string, title: string) {
-    this.toastr.error(message, title);
+    this.toastr.error(message, title, this.toastrConfig);
   }
 }
