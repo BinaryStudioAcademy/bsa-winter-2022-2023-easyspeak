8f4415df36c506c9b160f04a74d5077898ec19e5
Vladyslav Voropaiev <vladyslav.voropaiev1@nure.ua>
2023-03-14 11:56:04 +0200

    Add lessons module routing

--- frontend/src/app/modules/lessons/lessons-routing.module.ts
+++ frontend/src/app/modules/lessons/lessons-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LessonsPageComponent } from './lessons-page/lessons-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+    {
+        path: '',
+        component: LessonsPageComponent,
+    },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
