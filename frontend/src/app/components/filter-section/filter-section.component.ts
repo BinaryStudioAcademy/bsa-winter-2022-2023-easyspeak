b4743b0b5d77b42396b837a8e577c2dac707d471
ishootdice <voitenkobohdan@gmail.com>
2023-03-14 22:01:10 +0200

    Added util file and fixed size units for components

--- frontend/src/app/components/filter-section/filter-section.component.ts
+++ frontend/src/app/components/filter-section/filter-section.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { EventEmitter } from '@angular/core';
 import { Subject } from 'rxjs';
+import {topicsSample, langLevelsSample} from 'src/app/components/filter-section/filter-section.util'
 
 @Component({
   selector: 'app-filter-section',
@@ -16,91 +17,20 @@ export class FilterSectionComponent implements OnInit {
   public selectedLanguageFilters = new Set();
   public selectedTopicsFilters = new Set();
 
-  public topics = [
-    {
-      title: 'Traveling',
-    },
-    {
-      title: 'Music',
-    },
-    {
-      title: 'Films',
-    },
-    {
-      title: 'Cars',
-    },
-    {
-      title: 'Architecture',
-    },
-    {
-      title: 'Arts',
-    },
-    {
-      title: 'Celebreties',
-    }
-  ];
-
-  public langLevels = [
-    {
-      title: 'B1',
-      subtitle: 'Intermadiate',
-      desc: 'I am beginning to able to express myself clearly'
-    },
-    {
-      title: 'A1',
-      subtitle: 'Beginner',
-      desc: 'I am complete beginner'
-    },
-    {
-      title: 'A2',
-      subtitle: 'Pre-Intermadiate',
-      desc: 'I have some experience, but I still can not express myself properly'
-    },
-    {
-      title: 'B2',
-      subtitle: 'Upper-Intermadiate',
-      desc: 'I feel confident in using English'
-    },
-    {
-      title: 'C1',
-      subtitle: 'Advanced',
-      desc: 'I have a lot of experience and I feel that I can help someone to learn language'
-    },
-    {
-      title: 'C2',
-      subtitle: 'Proficient',
-      desc: 'I am fluent at English'
-    }
-  ]
+  public topics = topicsSample;
+  public langLevels = langLevelsSample;
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
-  selectLangLevel(title: any){
-    if(this.selectedLanguageFilters.has(title)){
-      this.selectedLanguageFilters.delete(title);
-    }
-    else{
-      this.selectedLanguageFilters.add(title);
-    }
+  removeLangLevel(title: any){
+    this.selectedLanguageFilters.delete(title);
   }
 
-  selectTopic(title: any){
-    if(this.selectedTopicsFilters.has(title)){
-      this.selectedTopicsFilters.delete(title);
-    }
-    else{
-      this.selectedTopicsFilters.add(title);
-    }
-  }
-
-  closeAllFilters(){
-    this.selectedLanguageFilters = new Set();
-    this.selectedTopicsFilters = new Set();
-
-    
+  removeTopic(title: any){
+    this.selectedTopicsFilters.delete(title);
   }
 
   updateLangFilters(eventData: Set<string>){
---
+++ frontend/src/app/components/filter-section/filter-section.util.ts
@@ -0,0 +1,83 @@
+ export var topicsSample = [
+    {
+        title: 'Traveling',
+    },
+    {
+        title: 'Arts',
+    },
+    {
+        title: 'Cars',
+    },
+    {
+        title: 'Celebreties',
+    },
+    {
+        title: 'Cooking',
+    },
+    {
+        title: 'Dancing',
+    },
+    {
+        title: 'Ecology',
+    },
+    {
+        title: 'Design'
+    },
+    {
+        title: 'History'
+    },
+    {
+        title: 'Fashion'
+    },
+    {
+        title: 'Medicine'
+    },
+    {
+        title: 'Technologies'
+    },
+    {
+        title: 'Pets'
+    },
+    {
+        title: 'Philosophy'
+    },
+    {
+        title: 'Photography'
+    },
+    {
+        title: 'Politics'
+    }
+  ];
+
+export var langLevelsSample = [
+    {
+      title: 'B1',
+      subtitle: 'Intermadiate',
+      desc: 'I am beginning to able to express myself clearly'
+    },
+    {
+      title: 'A1',
+      subtitle: 'Beginner',
+      desc: 'I am complete beginner'
+    },
+    {
+      title: 'A2',
+      subtitle: 'Pre-Intermadiate',
+      desc: 'I have some experience, but I still can not express myself properly'
+    },
+    {
+      title: 'B2',
+      subtitle: 'Upper-Intermadiate',
+      desc: 'I feel confident in using English'
+    },
+    {
+      title: 'C1',
+      subtitle: 'Advanced',
+      desc: 'I have a lot of experience and I feel that I can help someone to learn language'
+    },
+    {
+      title: 'C2',
+      subtitle: 'Proficient',
+      desc: 'I am fluent at English'
+    }
+  ]
