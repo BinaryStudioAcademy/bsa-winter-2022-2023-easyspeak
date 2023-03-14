3cb2d64186e9aa52e4525c51ed65b78a85f55325
Nadia <nadiamaksy4uk@gmail.com>
2023-03-15 00:33:24 +0200

    Add input forms

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,14 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { SharedModule } from '@shared/shared.module';
 
+import { AuthModule } from './modules/auth/auth.module';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
-import { AuthModule } from './modules/auth/auth.module';
 
 @NgModule({
     declarations: [AppComponent],
-    imports: [BrowserModule, SharedModule, AppRoutingModule, AuthModule],
+    imports: [BrowserModule, SharedModule, AppRoutingModule, AuthModule, BrowserAnimationsModule],
     providers: [],
     bootstrap: [AppComponent],
 })
