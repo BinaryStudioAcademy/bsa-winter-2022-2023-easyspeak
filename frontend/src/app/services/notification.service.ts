7899793d6688d17ce86b8523413c26b0289e7388
Vladyslav Voropaiev <vladyslav.voropaiev1@nure.ua>
2023-03-14 20:06:30 +0200

    Implement methods to show Success/Info/Error

--- frontend/src/app/services/notification.service.ts
+++ frontend/src/app/services/notification.service.ts
@@ -7,4 +7,16 @@ import { ToastrService } from 'ngx-toastr';
 export class NotificationService {
 
   constructor(private toastr: ToastrService) { }
+
+  showSuccess(message: string, title: string) {
+    this.toastr.success(message, title);
+  }
+
+  showInfo(message: string, title: string) {
+    this.toastr.info(message, title);
+  }
+
+  showError(message: string, title: string) {
+    this.toastr.error(message, title);
+  }
 }
