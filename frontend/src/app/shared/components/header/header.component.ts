25ce7f203eb7d462555440bcd049f83a2d3d8add
Olexandr Sidletsky <scasha2000@gmail.com>
2023-03-15 14:36:20 +0200

    remove global styles, check selected page by route

--- frontend/src/app/shared/components/header/header.component.ts
+++ frontend/src/app/shared/components/header/header.component.ts
@@ -9,20 +9,7 @@ export class HeaderComponent implements OnInit {
 
   constructor() { }
 
-  isSocialPageActive: boolean;
-  isTimetablePageActive: boolean;
-
   ngOnInit(): void {
-  }
-
-  setSocialActivePage() {
-    this.isSocialPageActive = true;
-    this.isTimetablePageActive = false;
-  }
-
-  setTimetableActivePage() {
-    this.isTimetablePageActive = true;
-    this.isSocialPageActive = false;
   }
 
 }
