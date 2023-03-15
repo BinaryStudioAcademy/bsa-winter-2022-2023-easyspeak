a8207a93ebfe76c87b5e9c3bf9afa0fb53f34eec
Kateryna Herasymiva <kateryna.herasymiva@gmail.com>
2023-03-15 07:52:10 +0200

    23: Added migration

---
+++ backend/EasySpeak.Core/EasySpeak.Core.DAL/Migrations/20230315055104_Added-contsrains.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EasySpeak.Core.DAL.Migrations
+{
+    public partial class Addedcontsrains : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "LanguageLevel",
+                table: "Lessons",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<long>(
+                name: "UserId",
+                table: "Lessons",
+                type: "bigint",
+                nullable: false,
+                defaultValue: 0L);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Lessons_UserId",
+                table: "Lessons",
+                column: "UserId",
+                unique: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Lessons_Users_UserId",
+                table: "Lessons",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "Id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Lessons_Users_UserId",
+                table: "Lessons");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Lessons_UserId",
+                table: "Lessons");
+
+            migrationBuilder.DropColumn(
+                name: "LanguageLevel",
+                table: "Lessons");
+
+            migrationBuilder.DropColumn(
+                name: "UserId",
+                table: "Lessons");
+        }
+    }
+}
