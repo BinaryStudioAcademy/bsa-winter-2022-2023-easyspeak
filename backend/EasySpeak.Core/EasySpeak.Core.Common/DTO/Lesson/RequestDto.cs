cb165626eab4fbe3d43b7290d1b8d133dafaa362
Kateryna Herasymiva <kateryna.herasymiva@gmail.com>
2023-03-15 21:57:20 +0200

    23: Added attributes

--- backend/EasySpeak.Core/EasySpeak.Core.Common/DTO/Lesson/RequestDto.cs
+++ backend/EasySpeak.Core/EasySpeak.Core.Common/DTO/Lesson/RequestDto.cs
@@ -4,11 +4,12 @@ namespace EasySpeak.Core.Common.DTO.Lesson;
 
 public class RequestDto
 {
-    [JsonProperty("languageLevels")]
-    public ICollection<LanguageLevelDto> LanguageLevels { get; set; }
+    [JsonProperty("languageLevels", Required = Required.Default)]
 
-    [JsonProperty("tags")]
-    public ICollection<TagForLessonDto> Tags { get; set; }
+    public ICollection<LanguageLevelDto>? LanguageLevels { get; set; }
+
+    [JsonProperty("tags", Required = Required.Default)]
+    public ICollection<TagForLessonDto>? Tags { get; set; }
 
     [JsonRequired]
     [JsonProperty("date")]
