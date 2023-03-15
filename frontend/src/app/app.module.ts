b4b8b0ba82a4798c060f08f5df4dde71e5a264bf
NadiaMaksymchuk <nadiamaksy4uk@gmail.com>
2023-03-15 22:48:00 +0200

    fix formatting

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -10,7 +10,15 @@ import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
     declarations: [AppComponent],
-    imports: [BrowserModule, SharedModule, ToastrModule.forRoot(), AppRoutingModule, AppRoutingModule, AuthModule, BrowserAnimationsModule],
+    imports: [
+        BrowserModule,
+        SharedModule,
+        ToastrModule.forRoot(),
+        AppRoutingModule,
+        AppRoutingModule,
+        AuthModule,
+        BrowserAnimationsModule,
+    ],
     providers: [],
     bootstrap: [AppComponent],
 })
