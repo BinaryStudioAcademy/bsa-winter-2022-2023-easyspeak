3c08bbf224defbba0cd5bc9504852b736c8bcf1b
Vladyslav Voropaiev <vladyslav.voropaiev1@nure.ua>
2023-03-15 19:35:19 +0200

    Add basic logic to lesson creation component

--- frontend/src/app/modules/lessons/lessons-create/lessons-create.component.ts
+++ frontend/src/app/modules/lessons/lessons-create/lessons-create.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
+import { ILesson } from '@shared/models/ILesson';
+
+import { LessonsService } from 'src/app/services/lessons.service';
 
 @Component({
-  selector: 'app-lessons-create',
-  templateUrl: './lessons-create.component.html',
-  styleUrls: ['./lessons-create.component.sass']
+    selector: 'app-lessons-create',
+    templateUrl: './lessons-create.component.html',
+    styleUrls: ['./lessons-create.component.sass'],
 })
 export class LessonsCreateComponent {
+    lessonToCreate: ILesson = {
+        name: '',
+        description: '',
+        mediaPath: '',
+        startsAt: new Date(),
+    };
 
+    constructor(private lessonService: LessonsService) {}
+
+    createLesson() {
+        console.log(this.lessonToCreate);
+        this.lessonService.createLesson(this.lessonToCreate);
+    }
 }
--- frontend/src/app/modules/lessons/lessons.module.ts
+++ frontend/src/app/modules/lessons/lessons.module.ts
@@ -4,6 +4,7 @@ import { FormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
+import { MatDatepickerModule } from '@angular/material/datepicker'
 
 import { LessonsCreateComponent } from './lessons-create/lessons-create.component';
 import { LessonsPageComponent } from './lessons-page/lessons-page.component';
@@ -21,6 +22,7 @@ import { LessonsRoutingModule } from './lessons-routing.module';
         FormsModule,
         MatInputModule,
         MatButtonModule,
+        MatDatepickerModule
     ],
 })
 export class LessonsModule { }
