25df44863184aaf35506289badb1be050810f4f2
Ihor Myroniuk <ihor.myroniuk@hotmail.com>
2023-03-16 15:08:06 +0200

    Integrate romote changes before pushing

---
+++ backend/EasySpeak.Core/EasySpeak.Core.WebAPI/Middlewares/FirebaseAuthMiddleware.cs
@@ -0,0 +1,43 @@
+﻿using EasySpeak.Core.BLL.Interfaces;
+using FirebaseAdmin.Auth;
+
+namespace EasySpeak.Core.WebAPI.Middlewares
+{
+    public class FirebaseAuthMiddleware
+    {
+        private readonly RequestDelegate next;
+
+        public FirebaseAuthMiddleware(RequestDelegate next)
+        {
+            this.next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext context, IFirebaseAuthService firebaseAuthService)
+        {
+            var authToken = context.Request.Headers["Authorization"].ToString();
+
+            if (!string.IsNullOrEmpty(authToken) && authToken.StartsWith("Bearer "))
+            {
+                var idToken = authToken.Substring("Bearer ".Length);
+
+                try
+                {
+                    var decodedToken = await FirebaseAuth.DefaultInstance.VerifyIdTokenAsync(idToken);
+
+                    var userEmail = decodedToken.Claims["email"].ToString();
+
+                    if (userEmail != null)
+                    {
+                        await firebaseAuthService.SetUserId(userEmail);
+                    }
+                }
+                catch (Exception)
+                {
+                    context.Response.StatusCode = StatusCodes.Status401Unauthorized;
+                    return;
+                }
+            }
+            await next(context);
+        }
+    }
+}
