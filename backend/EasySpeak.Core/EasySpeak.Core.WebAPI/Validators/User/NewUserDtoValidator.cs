9dc66e1ff05de306756ebb0b08c5119dcc0172fd
ShevchenkoKirilo <shevchenko.kirilo.kp01@gmail.com>
2023-03-17 14:55:47 +0200

    Created backend validation for user inputs

---
+++ backend/EasySpeak.Core/EasySpeak.Core.WebAPI/Validators/User/NewUserDtoValidator.cs
@@ -0,0 +1,22 @@
+﻿using EasySpeak.Core.Common.DTO;
+using EasySpeak.Core.DAL.Entities;
+using FluentValidation;
+
+namespace EasySpeak.Core.WebAPI.Validators
+{
+    public class NewUserDtoValidator : AbstractValidator<NewUserDto>
+    {
+        public NewUserDtoValidator()
+        {
+            RuleFor(u => u.Country).NotEmpty().IsEnumName(typeof(Country));
+            RuleFor(u => u.Language).NotEmpty().IsEnumName(typeof(Language));
+            RuleFor(u => u.Timezone).NotEmpty().IsEnumName(typeof(Timezone));
+            RuleFor(u => u.FirstName).NotEmpty().Matches(@"^[A-Za-z\s]*$").Length(3, 30);
+            RuleFor(u => u.LastName).NotEmpty().Matches(@"^[A-Za-z\s]*$").Length(3, 30);
+            RuleFor(u => u.Age).InclusiveBetween(6, 99);
+            RuleFor(u => u.Email).NotEmpty().EmailAddress();
+            RuleFor(u => u.Sex).NotEmpty().IsEnumName(typeof(Sex));
+            RuleFor(u => u.LanguageLevel).NotEmpty().IsEnumName(typeof(LanguageLevel));
+        }
+    }
+}
