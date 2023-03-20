fa36fbda88b38d8ebbe12b0df5daefff3754682f
bochka123 <sasha2003tkach001100@gmail.com>
2023-03-20 02:16:18 +0200

    fix eslint trouble

--- frontend/src/app/shared/shared.module.ts
+++ frontend/src/app/shared/shared.module.ts
@@ -1,18 +1,18 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatIconModule } from '@angular/material/icon';
 import { RouterModule } from '@angular/router';
 import { YouTubePlayerModule } from '@angular/youtube-player';
 
+import { ConfirmComponent } from './components/confirm/confirm.component';
 import { DropdownComponent } from './components/dropdown/dropdown.component';
 import { LoadingSpinnerComponent } from './components/loading-spinner/loading-spinner.component';
 import { ModalComponent } from './components/modal/modal.component';
 import { NotFoundComponent } from './components/not-found/not-found.component';
 import { YoutubePlayerComponent } from './components/youtube-player/youtube-player.component';
-import { ConfirmComponent } from './components/confirm/confirm.component';
-import {MatButtonModule} from "@angular/material/button";
 
 @NgModule({
     imports: [
