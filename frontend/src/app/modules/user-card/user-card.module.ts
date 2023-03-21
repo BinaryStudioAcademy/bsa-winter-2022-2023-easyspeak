0e650fbc3c39dee11e0b084ee61dd34431fa8119
Kateryna Herasymiva <kateryna.herasymiva@gmail.com>
2023-03-21 15:24:28 +0200

    47: Fixes analyzers issues

--- frontend/src/app/modules/user-card/user-card.module.ts
+++ frontend/src/app/modules/user-card/user-card.module.ts
@@ -1,7 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { UserCardComponent } from '@modules/user-card/user-card.component';
-import {MatProgressSpinnerModule} from "@angular/material/progress-spinner";
 
 @NgModule({
     declarations: [UserCardComponent],
