adfeedc460b677c2241e4dc8431370309fcc11a9
Mayank <90491471+NLMOTS@users.noreply.github.com>
2023-03-22 11:13:11 +0200

    Improvements
    Added
    -email validation message
    -password match validation

--- frontend/src/app/modules/forgot-password/chage-password/chage-password.component.ts
+++ frontend/src/app/modules/forgot-password/chage-password/chage-password.component.ts
@@ -1,10 +1,34 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-chage-password',
-  templateUrl: './chage-password.component.html',
-  styleUrls: ['./chage-password.component.sass']
+    selector: 'app-chage-password',
+    templateUrl: './chage-password.component.html',
+    styleUrls: ['./chage-password.component.sass'],
 })
 export class ChagePasswordComponent {
+    password: string = '';
 
+    repeatPassword: string = '';
+
+    showPassword: boolean = false;
+
+    showRepeatPassword: boolean = false;
+
+    passwordIsMatch: boolean = true;
+
+    togglePassword(inShow: boolean) {
+        this.showPassword = inShow;
+    }
+
+    toggleRepeatPassword(inShow: boolean) {
+        this.showRepeatPassword = inShow;
+    }
+
+    isSame() {
+        if (this.repeatPassword.length === 0) {
+            this.passwordIsMatch = true;
+        } else {
+            this.passwordIsMatch = this.password === this.repeatPassword;
+        }
+    }
 }
