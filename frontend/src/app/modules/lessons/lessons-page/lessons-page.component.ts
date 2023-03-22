88286c3b39a07a315ccc86816f39d8cf04776068
Vladyslav Voropaiev <vladyslav.voropaiev1@nure.ua>
2023-03-22 19:58:27 +0200

    Create basic logic on getting lessons

--- frontend/src/app/modules/lessons/lessons-page/lessons-page.component.ts
+++ frontend/src/app/modules/lessons/lessons-page/lessons-page.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
+import { langLevelsSample } from '@modules/filter-section/filter-section/filter-section.util';
 import { YoutubePlayerComponent } from '@shared/components/youtube-player/youtube-player.component';
+import { ILesson } from '@shared/models/lesson/ILesson';
+import { LessonsService } from 'src/app/services/lessons.service';
 
 import { LessonsCreateComponent } from '../lessons-create/lessons-create.component';
 
@@ -93,7 +96,7 @@ export class LessonsPageComponent implements OnInit {
         },
     ];
 
-    constructor(private dialogRef: MatDialog) { }
+    constructor(private dialogRef: MatDialog, private lessonService: LessonsService) { }
 
     ngOnInit(): void {
         const formatter = new Intl.DateTimeFormat('en-US', { day: 'numeric', month: 'long', year: 'numeric', weekday: 'long' });
@@ -122,5 +125,29 @@ export class LessonsPageComponent implements OnInit {
             height: '80%',
             width: '80%',
         });
+    }
+
+    getLessons() {
+        this.lessonService.getFilteredLessons({
+            languageLevels: [],
+            tags: [],
+            date: new Date('2023-04-03')
+        }).subscribe((response: ILesson[]) => {
+            response.forEach(lesson => {
+                this.lessons.push({
+                    imgPath: lesson.mediaPath,
+                    videoId: 'xqAriI87lFU',
+                    title: lesson.name,
+                    time: lesson.startAt.split('T')[1].substring(0, 5).replace(':', '.'),
+                    tutorAvatarPath: '../../../../assets/lesson-mocks/Photo )Patient).png',
+                    tutorFlagPath: '../../../../assets/lesson-icons/canada-test-flag.svg',
+                    tutorName: 'Roger Vaccaro',
+                    topics: lesson.tags.map((tag) => tag.name),
+                    viewersCount: lesson.subscribersCount,
+                    level: langLevelsSample[lesson.languageLevel].title,
+                    isDisabled: (new Date() > new Date(lesson.startAt)),
+                });
+            });
+         });
     }
 }
