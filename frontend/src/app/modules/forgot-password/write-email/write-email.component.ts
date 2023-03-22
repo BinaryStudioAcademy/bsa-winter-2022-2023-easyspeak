9e501dd6ec2b84c11653200ca3f1478ff13d1f3f
Mayank <90491471+NLMOTS@users.noreply.github.com>
2023-03-22 11:13:11 +0200

    Improvements
    Added
    -email validation message
    -password match validation

--- frontend/src/app/modules/forgot-password/write-email/write-email.component.ts
+++ frontend/src/app/modules/forgot-password/write-email/write-email.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-write-email',
-  templateUrl: './write-email.component.html',
-  styleUrls: ['./write-email.component.sass']
+    selector: 'app-write-email',
+    templateUrl: './write-email.component.html',
+    styleUrls: ['./write-email.component.sass'],
 })
 export class WriteEmailComponent {
+    emailPattern: RegExp = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
 
+    email: string = '';
+
+    isMatch: boolean = true;
+
+    checkMail(): void {
+        this.isMatch = this.emailPattern.test(this.email);
+    }
+
+    changedMail(): void {
+        this.isMatch = true;
+    }
 }
