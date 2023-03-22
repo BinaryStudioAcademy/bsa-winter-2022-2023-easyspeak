5b341b4769c21f6583258f78c577244ac0c887c4
Mayank <90491471+NLMOTS@users.noreply.github.com>
2023-03-22 11:13:11 +0200

    Improvements
    Added
    -email validation message
    -password match validation

--- frontend/src/app/modules/forgot-password/forgot-password.module.ts
+++ frontend/src/app/modules/forgot-password/forgot-password.module.ts
@@ -1,14 +1,16 @@
+import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { MatIconModule } from '@angular/material/icon';
 
-import { ForgotPasswordPageComponent } from './forgot-password-page/forgot-password-page.component';
-import { ForgotPasswordRoutingModule } from './forgot-password-routing.module';
-import { CheckEmailComponent } from './check-email/check-email.component';
 import { ChagePasswordComponent } from './chage-password/chage-password.component';
+import { CheckEmailComponent } from './check-email/check-email.component';
+import { ForgotPasswordPageComponent } from './forgot-password-page/forgot-password-page.component';
 import { WriteEmailComponent } from './write-email/write-email.component';
+import { ForgotPasswordRoutingModule } from './forgot-password-routing.module';
 
 @NgModule({
     declarations: [ForgotPasswordPageComponent, CheckEmailComponent, ChagePasswordComponent, WriteEmailComponent],
-    imports: [MatIconModule, ForgotPasswordRoutingModule],
+    imports: [MatIconModule, ForgotPasswordRoutingModule, CommonModule, FormsModule],
 })
 export class ForgotPasswordModule {}
