c84e678126bc1485ef446a75f4001b98616e9684
bochka123 <sasha2003tkach001100@gmail.com>
2023-03-26 11:47:48 +0300

    fixing eslint

--- frontend/src/app/modules/auth/auth.module.ts
+++ frontend/src/app/modules/auth/auth.module.ts
@@ -3,6 +3,7 @@ import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { JWT_OPTIONS, JwtHelperService } from '@auth0/angular-jwt';
 import { MaterialModule } from '@shared/material/material.module';
+import { SharedModule } from '@shared/shared.module';
 
 import { AuthGuard } from '../../core/guards/auth.guard';
 import { AuthorizedUsersGuard } from '../../core/guards/authorized-users.guard';
@@ -11,7 +12,6 @@ import { AuthPageComponent } from './auth-page/auth-page.component';
 import { SignInComponent } from './sign-in/sign-in.component';
 import { SignUpComponent } from './sign-up/sign-up.component';
 import { AuthRoutingModule } from './auth-routing.module';
-import {SharedModule} from "@shared/shared.module";
 
 @NgModule({
     declarations: [SignUpComponent, SignInComponent, AuthPageComponent],
