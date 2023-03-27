948feefea9f1c7123fbb50651d03115be2de6006
Vladyslav Voropaiev <vladyslav.voropaiev1@nure.ua>
2023-03-27 16:11:52 +0300

    Implement getLessonsCount method

--- frontend/src/app/services/lessons.service.ts
+++ frontend/src/app/services/lessons.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpService } from '@core/services/http.service';
+import { IIDayCard } from '@shared/models/lesson/IDayCard';
 import { IFilter } from '@shared/models/lesson/IFilter';
 import { ILesson } from '@shared/models/lesson/ILesson';
 import { INewLesson } from '@shared/models/lesson/INewLesson';
@@ -18,6 +19,10 @@ export class LessonsService {
 
     getFilteredLessons(filter: IFilter) {
         return this.http.post<ILesson[]>(`${this.routePrefix}/filters`, filter);
+    }
+
+    getLessonsCount(date: string) {
+        return this.http.get<IIDayCard>(`${this.routePrefix}/week/?Date=${date}`);
     }
 
     getQuestions(id: number) {
