a5d9e617c0efe007988ae15d7b0c12a37c982411
ShevchenkoKirilo <shevchenko.kirilo.kp01@gmail.com>
2023-03-29 14:05:44 +0300

    reworked lessons page so there are no empty space when questions opened

--- frontend/src/app/modules/lessons/lessons-page/lessons-page.component.ts
+++ frontend/src/app/modules/lessons/lessons-page/lessons-page.component.ts
@@ -27,7 +27,11 @@ export class LessonsPageComponent implements OnInit, OnChanges {
 
     lessons: Lesson[];
 
-    constructor(private dialogRef: MatDialog, private lessonService: LessonsService) { }
+    lessonsColumn1: Lesson[];
+
+    lessonsColumn2: Lesson[];
+
+    constructor(private dialogRef: MatDialog, private lessonService: LessonsService) {}
 
     ngOnInit(): void {
         this.selectedDateFilter = new Date();
@@ -65,31 +69,50 @@ export class LessonsPageComponent implements OnInit, OnChanges {
     }
 
     getLessons() {
-        this.lessonService.getFilteredLessons({
-            languageLevels: this.selectedLanguageFilters.map((level: string) => Object.values(LanguageLevels).indexOf(level)),
-            tags: this.selectedInterestsFilters.map((topic) => ({ name: topic })),
-            date: new Date(this.selectedDateFilter.toISOString().slice(0, 10)),
-        }).subscribe((response: ILesson[]) => {
-            this.lessons = [];
+        this.lessonService
+            .getFilteredLessons({
+                languageLevels: this.selectedLanguageFilters.map((level: string) =>
+                    Object.values(LanguageLevels).indexOf(level)),
+                tags: this.selectedInterestsFilters.map((topic) => ({ name: topic })),
+                date: new Date(this.selectedDateFilter.toISOString().slice(0, 10)),
+            })
+            .subscribe((response: ILesson[]) => {
+                this.lessons = [];
 
-            //TODO: When everything regarding user in the database is finished, update the lesson retrieval code
+                //TODO: When everything regarding user in the database is finished, update the lesson retrieval code
 
-            response.forEach(lesson => {
-                this.lessons.push({
-                    id: lesson.id,
-                    imgPath: lesson.mediaPath,
-                    videoId: 'xqAriI87lFU',
-                    title: lesson.name,
-                    time: lesson.startAt.split('T')[1].substring(0, 5).replace(':', '.'),
-                    tutorAvatarPath: '../../../../assets/lesson-mocks/Photo )Patient).png',
-                    tutorFlagPath: '../../../../assets/lesson-icons/canada-test-flag.svg',
-                    tutorName: 'Roger Vaccaro',
-                    topics: lesson.tags.map((tag) => tag.name),
-                    subscribersCount: lesson.subscribersCount,
-                    level: langLevelsSample[lesson.languageLevel].title,
-                    isDisabled: (new Date() > new Date(lesson.startAt)),
+                response.forEach((lesson) => {
+                    this.lessons.push({
+                        id: lesson.id,
+                        imgPath: lesson.mediaPath,
+                        videoId: 'xqAriI87lFU',
+                        title: lesson.name,
+                        time: lesson.startAt.split('T')[1].substring(0, 5).replace(':', '.'),
+                        tutorAvatarPath: '../../../../assets/lesson-mocks/Photo )Patient).png',
+                        tutorFlagPath: '../../../../assets/lesson-icons/canada-test-flag.svg',
+                        tutorName: 'Roger Vaccaro',
+                        topics: lesson.tags.map((tag) => tag.name),
+                        subscribersCount: lesson.subscribersCount,
+                        level: langLevelsSample[lesson.languageLevel].title,
+                        isDisabled: new Date() > new Date(lesson.startAt),
+                    });
                 });
+                this.splitLessons();
             });
+    }
+
+    splitLessons() {
+        this.lessonsColumn1 = [];
+        this.lessonsColumn2 = [];
+        let index = 0;
+
+        this.lessons.forEach((lesson) => {
+            if (index % 2 === 0) {
+                this.lessonsColumn1.push(lesson);
+            } else {
+                this.lessonsColumn2.push(lesson);
+            }
+            index++;
         });
     }
 
