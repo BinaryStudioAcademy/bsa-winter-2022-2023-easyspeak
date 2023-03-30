224d70b232f9b0a1735e6c6d5456920a83a393e5
NadiaMaksymchuk <nadiamaksy4uk@gmail.com>
2023-03-30 21:13:25 +0300

    get real info in header completed

--- frontend/src/app/shared/components/header/header.component.ts
+++ frontend/src/app/shared/components/header/header.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { AuthService } from '@core/services/auth.service';
+import { IUserInfo } from '@shared/models/IUserInfo';
 
 import { UserNotificationComponent } from '../user-notification/user-notification.component';
 
@@ -11,13 +12,23 @@ import { UserNotificationComponent } from '../user-notification/user-notificatio
 export class HeaderComponent implements OnInit {
     @ViewChild('notificationsMenu') notificationsMenu: UserNotificationComponent;
 
+    userInfo: IUserInfo;
+
     constructor(private authService: AuthService) {}
 
     ngOnInit(): void {
         if (this.authService.isAuthenticated()) {
             this.authService.setUserSection();
         }
+
+        const userSection = this.authService.getUserSection();
+
+        if (userSection) {
+            this.userInfo = userSection;
+        }
     }
+
+    getFullName = () => `${this.userInfo.firstName} ${this.userInfo.lastName}`;
 
     logOut() {
         this.authService.logout();
