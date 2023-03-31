6c34f63436dfe3eb43a6ee27d9d020f4cf682a8c
Mayank <90491471+NLMOTS@users.noreply.github.com>
2023-03-31 09:37:14 +0300

    Added toastr notification to user

--- frontend/src/app/modules/forgot-password/change-password/change-password.component.ts
+++ frontend/src/app/modules/forgot-password/change-password/change-password.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { AuthService } from '@core/services/auth.service';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
     selector: 'app-change-password',
@@ -26,7 +27,7 @@ export class ChangePasswordComponent implements OnInit {
 
     responseIsOk: boolean = true;
 
-    constructor(private router: Router, private route: ActivatedRoute, private authService: AuthService) {}
+    constructor(private router: Router, private route: ActivatedRoute, private authService: AuthService, private toastr: ToastrService) {}
 
     ngOnInit(): void {
         this.route.queryParams.subscribe((params) => {
@@ -57,8 +58,13 @@ export class ChangePasswordComponent implements OnInit {
 
             if ((await response) === true) {
                 this.authService.signIn(this.email, this.password);
+
+                this.toastr.success('Password have been changed successfully!', 'Success');
+
                 this.router.navigate(['main']);
             } else {
+                this.toastr.error('This link time has expired!', 'Error');
+
                 this.responseIsOk = false;
             }
         }
