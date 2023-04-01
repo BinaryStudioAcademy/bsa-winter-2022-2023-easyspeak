5d1c59381366d7588817200ff3a95f1e7865f1dc
NadiaMaksymchuk <nadiamaksy4uk@gmail.com>
2023-04-01 20:25:20 +0300

    fix comment

--- frontend/src/app/shared/components/header/header.component.ts
+++ frontend/src/app/shared/components/header/header.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { AuthService } from '@core/services/auth.service';
+import { UserService } from '@core/services/user.service';
 import { IUserInfo } from '@shared/models/IUserInfo';
+import { Subject } from 'rxjs';
 
 import { UserNotificationComponent } from '../user-notification/user-notification.component';
 
@@ -12,22 +14,26 @@ import { UserNotificationComponent } from '../user-notification/user-notificatio
 export class HeaderComponent implements OnInit {
     @ViewChild('notificationsMenu') notificationsMenu: UserNotificationComponent;
 
-    currentUser: IUserInfo;
+    currentUser = new Subject<IUserInfo>();
 
-    constructor(private authService: AuthService) {}
+    userFullName: string;
+
+    constructor(private authService: AuthService, private userService: UserService) {}
 
     ngOnInit(): void {
         if (this.authService.isAuthenticated()) {
             this.authService.setUserSection();
-            const userSection = this.authService.getUserSection();
-
-            if (userSection) {
-                this.currentUser = userSection;
-            }
         }
+
+        this.currentUser.subscribe(
+            (resp) => this.getFullName(resp),
+        );
+        this.userService.getUser().subscribe(this.currentUser);
     }
 
-    getFullName = () => `${this.currentUser.firstName} ${this.currentUser.lastName}`;
+    getFullName(userInfo: IUserInfo) {
+        this.userFullName = `${userInfo.firstName} ${userInfo.lastName}`;
+    }
 
     logOut() {
         this.authService.logout();
