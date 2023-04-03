cc7077359793a8b1ad7b6cda4c64fd9cb0499376
NadiaMaksymchuk <nadiamaksy4uk@gmail.com>
2023-04-03 15:30:52 +0300

    ChatService implementation

---
+++ backend/EasySpeak.Core/EasySpeak.Core.BLL/Services/ChatService.cs
@@ -0,0 +1,37 @@
+﻿using AutoMapper;
+using EasySpeak.Core.BLL.Interfaces;
+using EasySpeak.Core.Common.DTO.Message;
+using EasySpeak.Core.DAL.Context;
+using Microsoft.EntityFrameworkCore;
+using System.Runtime.CompilerServices;
+
+namespace EasySpeak.Core.BLL.Services
+{
+    public class ChatService : BaseService
+    {
+        private readonly IFirebaseAuthService _firebaseAuthService;
+        public ChatService(EasySpeakCoreContext context, IMapper mapper, IFirebaseAuthService firebaseAuthService) : base(context, mapper)
+        {
+            _firebaseAuthService = firebaseAuthService;
+        }
+
+        public async Task<ICollection<ChatPersonDto>> GetUnreadAndLastSendMessageAsync() =>
+            await _context.Chats.Where(chat => chat.Users
+            .FirstOrDefault(user => user.Id == _firebaseAuthService.UserId) != null 
+            && chat.Messages.Any(message => message.IsRead == false))
+            .Select(chat =>
+                        new ChatPersonDto
+                        {
+                            Name = chat.Users
+                            .First(user => user.Id == _firebaseAuthService.UserId).FirstName + " " + chat.Users
+                            .First(user => user.Id == _firebaseAuthService.UserId).LastName,
+                            LastMessage = chat.Messages.Last(message => message.IsRead == false).Text,
+                            NumberOfUnreadMessages = (uint)chat.Messages.Count(message => message.IsRead == false)
+                        })
+                .ToListAsync();
+
+        public async Task<ICollection<MessageGroupDto>> GetMessagesInChat(int chatId) =>
+            await _context.Chats.FirstOrDefaultAsync(chat => chat.Id == chatId)
+            .Select()
+    }
+}
---
+++ backend/EasySpeak.Core/EasySpeak.Core.Common/DTO/Message/ChatPersonDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasySpeak.Core.Common.DTO.Message
+{
+    public class ChatPersonDto
+    {
+        public string Name { get; set; } = string.Empty;
+        public bool IsOnline { get; set; }
+        public string LastMessage { get; set; } = string.Empty;
+        public uint NumberOfUnreadMessages { get; set; }
+    }
+}
