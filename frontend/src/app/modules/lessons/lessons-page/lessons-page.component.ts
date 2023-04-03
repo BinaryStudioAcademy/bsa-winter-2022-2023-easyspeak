cdee883834770d7f40f7eec96e27c725879ba76f
Potapchuk Anna <potapchuk.anna.kp.01@gmail.com>
2023-04-03 19:41:29 +0300

    two columns lecture returned

--- frontend/src/app/modules/lessons/lessons-page/lessons-page.component.ts
+++ frontend/src/app/modules/lessons/lessons-page/lessons-page.component.ts
@@ -29,6 +29,10 @@ export class LessonsPageComponent implements OnInit, OnChanges {
 
     lessons: Lesson[];
 
+    lessonsColumn1: Lesson[];
+
+    lessonsColumn2: Lesson[];
+
     constructor(private dialogRef: MatDialog, private lessonService: LessonsService) {}
 
     ngOnInit(): void {
@@ -82,6 +86,8 @@ export class LessonsPageComponent implements OnInit, OnChanges {
             })
             .subscribe((response) => {
                 this.lessons = this.mapLessons(response);
+                this.lessonsColumn1 = this.lessons.filter((el, index) => index % 2 === 0);
+                this.lessonsColumn2 = this.lessons.filter((el, index) => index % 2 === 1);
             });
     }
 
