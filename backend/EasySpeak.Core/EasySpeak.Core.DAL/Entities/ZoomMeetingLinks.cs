4a2b57bf8127d548a084c40da06f398375884de5
Vladyslav Voropaiev <vladyslav.voropaiev1@nure.ua>
2023-04-04 16:09:38 +0300

    Create ZoomMeetingLinks class

---
+++ backend/EasySpeak.Core/EasySpeak.Core.DAL/Entities/ZoomMeetingLinks.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasySpeak.Core.DAL.Entities
+{
+    public class ZoomMeetingLinks
+    {
+        string JoinUrl { get; set; } = string.Empty;
+        string HostUrl { get; set; } = string.Empty;
+    }
+}
