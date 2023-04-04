0767d611eadce5df967349ce49fafd4347950a5d
Vladyslav Voropaiev <vladyslav.voropaiev1@nure.ua>
2023-04-04 16:10:00 +0300

    Create IZoomApiService interface

---
+++ backend/EasySpeak.Core/EasySpeak.Core.BLL/Interfaces/IZoomApiService.cs
@@ -0,0 +1,10 @@
+﻿using EasySpeak.Core.DAL.Entities;
+
+namespace EasySpeak.Core.BLL.Interfaces
+{
+    public interface IZoomApiService
+    {
+        Task<ZoomMeetingLinks> GetMeetingLinks();
+        string GenerateAccessToken(string apiKey, string apiSecret);
+    }
+}
