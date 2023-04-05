b2bf904aecbec7026e889c62f0ef498213d4b08f
Vladyslav Voropaiev <vladyslav.voropaiev1@nure.ua>
2023-04-05 15:29:27 +0300

    Fix code smells

--- backend/EasySpeak.Core/EasySpeak.Core.BLL/Services/ZoomApiService.cs
+++ backend/EasySpeak.Core/EasySpeak.Core.BLL/Services/ZoomApiService.cs
@@ -59,7 +59,7 @@ public class ZoomApiService : IZoomApiService
         }
         else
         {
-            throw new ArgumentNullException("Failed to generate Zoom meeting links.");
+            throw new ArgumentNullException();
         }
     }
 
