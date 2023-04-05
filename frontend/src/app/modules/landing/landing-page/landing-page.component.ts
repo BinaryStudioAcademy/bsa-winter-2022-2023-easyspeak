e73dc042cb1b788133abecfa59b2d64959edcfd2
Mayank <90491471+NLMOTS@users.noreply.github.com>
2023-04-05 15:50:38 +0300

    Removed extra code

--- frontend/src/app/modules/landing/landing-page/landing-page.component.ts
+++ frontend/src/app/modules/landing/landing-page/landing-page.component.ts
@@ -1,27 +1,8 @@
 import { Component } from '@angular/core';
-import { UserCard } from '@shared/models/user/user-card';
 
 @Component({
     selector: 'app-landing-page',
     templateUrl: './landing-page.component.html',
     styleUrls: ['./landing-page.component.sass'],
 })
-export class LandingPageComponent {
-    userT: UserCard = {
-        name: 'Alfredo Culhane',
-
-        country: 'Canada',
-
-        language: 'English',
-
-        imagePath: '../../../assets/user-card-icons/Man Curly Hair.svg',
-
-        languageLevel: 'B1',
-
-        tags: ['healthy', 'food', 'diet', 'sport', 'biology'],
-
-        status: 70,
-
-        flag: '../../../assets/user-card-icons/canada-flag.svg',
-    };
-}
+export class LandingPageComponent {}
