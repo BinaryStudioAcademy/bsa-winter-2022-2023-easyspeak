27844a80b85df36752aadd136f45027b36e26de8
Mayank <90491471+NLMOTS@users.noreply.github.com>
2023-04-11 14:56:03 +0300

    Removed subscription section in user detail tab

--- frontend/src/app/modules/user-profile/user-profile.module.ts
+++ frontend/src/app/modules/user-profile/user-profile.module.ts
@@ -5,14 +5,12 @@ import { SharedModule } from '@shared/shared.module';
 import { UserDetailsComponent } from './user-details/user-details/user-details.component';
 import { UserPasswordChangeComponent } from './user-password-change/user-password-change.component';
 import { UserProfilePageComponent } from './user-profile-page/user-profile-page.component';
-import { UserSubscriptionComponent } from './user-subscription/user-subscription.component';
 import { UserProfileRoutingModule } from './user-profile-routing.module';
 
 @NgModule({
     declarations: [
         UserProfilePageComponent,
         UserDetailsComponent,
-        UserSubscriptionComponent,
         UserPasswordChangeComponent,
     ],
     imports: [
