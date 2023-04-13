6dca0af9c93a66e3c312d409d54e3b6027e530bf
Vladyslav Voropaiev <vladyslav.voropaiev1@nure.ua>
2023-04-13 11:56:36 +0300

    Fix language level mapper cases

--- frontend/src/app/shared/data/LanguageLevelMapper.ts
+++ frontend/src/app/shared/data/LanguageLevelMapper.ts
@@ -2,13 +2,13 @@ import { LanguageLevel } from './languageLevel';
 
 export function mapLanguageLevelToString(level: string): string {
     switch (level) {
-        case 'B1':
+        case LanguageLevel.B1:
             return 'B1 (Intermediate)';
-        case 'B2':
+        case LanguageLevel.B2:
             return 'B2 (Upper-Intermediate)';
-        case 'C1':
+        case LanguageLevel.C1:
             return 'C1 (Advanced)';
-        case 'C2':
+        case LanguageLevel.C2:
             return 'C2 (Proficient)';
         default:
             throw new Error(`Invalid language level: ${level}`);
